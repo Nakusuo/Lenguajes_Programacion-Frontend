0c54130f4fef76488b9a00c577a14602f6ead460
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-04-12 14:49:04 -0500

    refactor: Cambie el nombre de priceUnit a unitPrice y actualice los parámetros del constructor para mayor claridad.

--- minerva/src/main/java/com/minerva/domain/entities/product/StockEntry.java
+++ minerva/src/main/java/com/minerva/domain/entities/product/StockEntry.java
@@ -14,16 +14,24 @@ public class StockEntry {
     private final UUID stockEntryId;
     private final ProductId productNameId;
     private final SupplierId supplierNameId;
-    private final Money priceUnit;
+    private final Money unitPrice;
     private final ProductQuantity quantity;
     private final LocalDateTime expirationDate;
     private final LocalDateTime registrationDate;
 
-    private StockEntry(UUID stockEntryId, ProductId productNameId, SupplierId supplierNameId, Money priceUnit, ProductQuantity quantity, LocalDateTime expirationDate, LocalDateTime registrationDate) {
+    private StockEntry(
+            UUID stockEntryId,
+            ProductId productId,
+            SupplierId supplierId,
+            Money unitPrice,
+            ProductQuantity quantity,
+            LocalDateTime expirationDate,
+            LocalDateTime registrationDate)
+    {
         this.stockEntryId = stockEntryId;
-        this.productNameId = productNameId;
-        this.supplierNameId = supplierNameId;
-        this.priceUnit = priceUnit;
+        this.productNameId = productId;
+        this.supplierNameId = supplierId;
+        this.unitPrice = unitPrice;
         this.quantity = quantity;
         this.expirationDate = expirationDate;
         this.registrationDate = registrationDate;
@@ -31,34 +39,35 @@ public class StockEntry {
 
     public static StockEntry restore(
         UUID stockEntryId,
-        String productNameId,
-        String supplierNameId,
-        BigDecimal priceUnit,
+        String productId,
+        String supplierId,
+        BigDecimal unitPrice,
         BigDecimal quantity,
         LocalDateTime expirationDate,
         LocalDateTime registrationDate) 
     {
-    ProductId productId = ProductId.of(productNameId).getData();
-    SupplierId supplierId = SupplierId.of(supplierNameId).getData();
-    Money unitPrice = Money.of(priceUnit).getData();
-    ProductQuantity productQuantity = ProductQuantity.of(quantity).getData();
 
     return new StockEntry(
             stockEntryId,
-            productId,
-            supplierId,
-            unitPrice,
-            productQuantity,
+            ProductId.of(productId).getData(),
+            SupplierId.of(supplierId).getData(),
+            Money.of(unitPrice).getData(),
+            ProductQuantity.of(quantity).getData(),
             expirationDate,
             registrationDate
     );
 }
 
-    static Result<StockEntry> create(ProductId productNameId, SupplierId supplierNameId, Money priceUnit, ProductQuantity quantity, LocalDateTime expirationDate) {
-
+    static Result<StockEntry> create(
+            ProductId productNameId,
+            SupplierId supplierNameId,
+            Money unitPrice,
+            ProductQuantity quantity,
+            LocalDateTime expirationDate
+    ) {
         if (productNameId == null) return Result.fail("El nombre del producto no puede estar vacío.");
         if (supplierNameId == null) return Result.fail("El nombre del proveedor no puede estar vacío.");
-        if (priceUnit != null && priceUnit.isZeroOrLess()) return Result.fail("El precio del producto debe ser mayor a 0.");
+        if (unitPrice != null && unitPrice.isZeroOrLess()) return Result.fail("El precio del producto debe ser mayor a 0.");
         if (quantity != null && quantity.isZeroOrLess()) return Result.fail("La cantidad del producto debe ser mayor a 0.");
 
         if (expirationDate != null && (expirationDate.isBefore(LocalDateTime.now()) || expirationDate.isEqual(LocalDateTime.now())))
@@ -67,7 +76,7 @@ public class StockEntry {
         UUID stockEntryId = UUID.randomUUID();
         LocalDateTime registrationDate = LocalDateTime.now();
 
-        StockEntry stockEntry = new StockEntry(stockEntryId, productNameId, supplierNameId, priceUnit, quantity, expirationDate, registrationDate);
+        StockEntry stockEntry = new StockEntry(stockEntryId, productNameId, supplierNameId, unitPrice, quantity, expirationDate, registrationDate);
 
         return Result.success(stockEntry);
     }
@@ -84,8 +93,8 @@ public class StockEntry {
         return supplierNameId;
     }
 
-    public Money getPriceUnit() {
-        return priceUnit;
+    public Money getUnitPrice() {
+        return unitPrice;
     }
 
     public ProductQuantity getQuantity() {
