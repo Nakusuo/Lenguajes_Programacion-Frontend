3b411300def4841db26ba2ae340a362c551501bc
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-05-27 11:06:22 -0500

    productRepository

--- minerva/src/main/java/com/minerva/infrastructure/adapter/ProductRepositoryAdapter.java
+++ minerva/src/main/java/com/minerva/infrastructure/adapter/ProductRepositoryAdapter.java
@@ -12,6 +12,7 @@ import com.minerva.infrastructure.persistence.repository.JpaStockEntryRepository
 import com.minerva.infrastructure.persistence.repository.JpaUnitToBulkRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.time.LocalDateTime;
@@ -32,72 +33,61 @@ public class ProductRepositoryAdapter implements ProductRepository {
         this.jpaUnitToBulkRepository = jpaUnitToBulkRepository;
     }
 
+    @Transactional
+    @Override
+    public void registerProduct(Product product, StockEntry stockEntry) {
+        save(product);
+        save(stockEntry);
+    }
 
     @Override
     public void save(Product product) {
-        jpaProductRepository.save(new ProductEntity(
-                product.getNameId().value(),
-                product.getGainStrategy(),
-                product.getGainAmount().value(),
-                product.getStock().value(),
-                product.getReorderLevel().get().value(),
-                product.getBarCode().get().value(),
-                product.getSaleType(),
-                product.getCategory(),
-                product.getRegistrationDate()                
-        ));
+        jpaProductRepository.save(toEntity(product));
     }
 
     @Override
     public void save(StockEntry stockEntry) {
-        ProductEntity productEntity = entityManager.getReference(ProductEntity.class, stockEntry.getProductNameId().value());
-        SupplierEntity supplierEntity = entityManager.getReference(SupplierEntity.class, stockEntry.getSupplierNameId().value());
-
-        jpaStockEntryRepository.save(new StockEntryEntity(
-                stockEntry.getId(),
-                productEntity,
-                supplierEntity,
-                stockEntry.getUnitPrice().value(),
-                stockEntry.getQuantity().value(),
-                stockEntry.getExpirationDate(),
-                stockEntry.getRegistrationDate()
-        ));
+        jpaStockEntryRepository.save(toEntity(stockEntry));
     }
 
     @Override
     public void saveUnitToBulk(ProductId unitProductId, ProductId bulkProductId, ProductQuantity quantity) {
-        ProductEntity unitProductEntity = entityManager.getReference(ProductEntity.class, unitProductId.value());
-        ProductEntity bulkProductEntity = entityManager.getReference(ProductEntity.class, bulkProductId.value());
+        ProductEntity unitProductEntity = entityManager.getReference(ProductEntity.class, unitProductId.value);
+        ProductEntity bulkProductEntity = entityManager.getReference(ProductEntity.class, bulkProductId.value);
 
         jpaUnitToBulkRepository.save(new UnitToBulkEntity(
-                new UnitToBulkEntity.UnitToBulkId(unitProductId.value(), bulkProductId.value()),
+                new UnitToBulkEntity.UnitToBulkId(unitProductId.value, bulkProductId.value),
                 unitProductEntity,
                 bulkProductEntity,
-                quantity.value(),
+                quantity.value,
                 LocalDateTime.now()
         ));
     }
 
     @Override
     public boolean existsById(ProductId id) {
-        return jpaProductRepository.existsById(id.value());
+        return jpaProductRepository.existsById(id.value);
     }
 
     @Override
     public boolean existByBarCode(BarCode barCode) {
-        return jpaProductRepository.existsByBarCode(barCode.value());
+        return jpaProductRepository.existsByBarCode(barCode.value);
     }
 
     @Override
     public Optional<Product> findById(ProductId id) {
-        
+        return jpaProductRepository.findById(id.value)
+                .map(this::toDomain);
     }
 
     @Override
     public Optional<Product> findByBarCode(BarCode barCode) {
-        return Optional.empty();
+        return jpaProductRepository.findByBarCode(barCode.value)
+                .map(this::toDomain);
     }
 
+    // OJAZOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+    // ESTO FALTA
     @Override
     public Optional<StockEntry> findLatestEntryBeforeToday(ProductId id) {
         return Optional.empty();
@@ -105,11 +95,77 @@ public class ProductRepositoryAdapter implements ProductRepository {
 
     @Override
     public List<Product> findAllProducts() {
-        
+        return jpaProductRepository.findAll()
+                .stream()
+                .map(this::toDomain)
+                .toList();
     }
 
     @Override
     public List<StockEntry> findAllEntriesByProductId(ProductId id) {
-        return List.of();
+        return jpaStockEntryRepository.findByProduct_ProductNameId(id.value)
+                .stream()
+                .map(this::toDomain)
+                .toList();
+    }
+
+    private Product toDomain(ProductEntity entity) {
+        return new Product(
+                entity.getProductNameId(),
+                entity.getGainStrategy(),
+                entity.getGainAmount(),
+                entity.getReorderLevel(),
+                entity.getBarCode(),
+                entity.getSaleType(),
+                entity.getStock(),
+                entity.getCategory(),
+                entity.getPrice(),
+                entity.getRegistrationDate()
+        );
+    }
+
+    private ProductEntity toEntity(Product product) {
+        return new ProductEntity(
+                product.getNameId().value,
+                product.getGainStrategy(),
+                product.getGainAmount().value,
+                product.getPrice().value,
+                product.getStock().value,
+                product.getReorderLevel().get().value,
+                product.getBarCode().get().value,
+                product.getSaleType(),
+                product.getCategory(),
+                product.getRegistrationDate()
+        );
+    }
+
+    private StockEntryEntity toEntity(StockEntry stockEntry) {
+        ProductEntity productEntity =
+                entityManager.getReference(ProductEntity.class, stockEntry.getProductNameId().value);
+
+        SupplierEntity supplierEntity =
+                entityManager.getReference(SupplierEntity.class, stockEntry.getSupplierNameId().value);
+
+        return new StockEntryEntity(
+                stockEntry.getStockEntryId(),
+                productEntity,
+                supplierEntity,
+                stockEntry.getUnitPrice().value,
+                stockEntry.getQuantity().value,
+                stockEntry.getExpirationDate().orElse(null),
+                stockEntry.getRegistrationDate()
+        );
+    }
+
+    private StockEntry toDomain(StockEntryEntity entity) {
+        return new StockEntry(
+                entity.getStockEntryId(),
+                entity.getProductEntity().getProductNameId(),
+                entity.getSupplierEntity().getSupplierNameId(),
+                entity.getUnitPrice(),
+                entity.getQuantity(),
+                entity.getExpirationDate(),
+                entity.getRegistrationDate()
+        );
     }
 }
