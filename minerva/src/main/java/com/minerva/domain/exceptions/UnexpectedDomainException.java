de16b3845adc2e94b76f5a60235754de8c7d1696
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-05-27 10:06:19 -0500

    exception agregada

---
+++ minerva/src/main/java/com/minerva/domain/exceptions/UnexpectedDomainException.java
@@ -0,0 +1,8 @@
+package com.minerva.domain.exceptions;
+
+public class UnexpectedDomainException extends RuntimeException {
+
+    public UnexpectedDomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
