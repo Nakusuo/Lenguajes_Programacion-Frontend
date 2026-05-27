641386f01df0cfb71fbc7f5708fb4a1d1535b1f3
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-05-27 11:55:46 -0500

    nuevas funciones

--- minerva/src/main/java/com/minerva/infrastructure/persistence/repository/JpaSupplierRepository.java
+++ minerva/src/main/java/com/minerva/infrastructure/persistence/repository/JpaSupplierRepository.java
@@ -1,13 +1,16 @@
 package com.minerva.infrastructure.persistence.repository;
 
 import com.minerva.infrastructure.persistence.entity.SupplierEntity;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaSupplierRepository extends JpaRepository<SupplierEntity, String> {
-    SupplierEntity findByRuc(String ruc);
-    SupplierEntity findByPhoneNumber(String phoneNumber);
+    Optional<SupplierEntity> findByRuc(String ruc);
+    Optional<SupplierEntity> findByPhoneNumber(String phoneNumber);
     boolean existsByRuc(String ruc);
     boolean existsByPhoneNumber(String phoneNumber);
 }
