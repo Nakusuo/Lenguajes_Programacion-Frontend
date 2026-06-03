1632dfcebd457f396f191c28c2513a47f8303568
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-03 18:53:17 -0500

    evita nulos

--- minerva/src/main/java/com/minerva/domain/entities/product/InventoryLossId.java
+++ minerva/src/main/java/com/minerva/domain/entities/product/InventoryLossId.java
@@ -18,6 +18,9 @@ public class InventoryLossId {
 
     public static InventoryLossId fromString(String value) throws DomainException {
         try {
+            if (value == null || value.isEmpty()) {
+                throw new DomainException("El ID de pérdida de inventario no puede ser nulo o vacío.");
+            }
             UUID.fromString(value);
         } catch (IllegalArgumentException e) {
             throw new DomainException("El ID de pérdida de inventario no tiene un formato válido: " + value);
--- minerva/src/main/java/com/minerva/domain/entities/sale/ProductReturnId.java
+++ minerva/src/main/java/com/minerva/domain/entities/sale/ProductReturnId.java
@@ -18,6 +18,9 @@ public class ProductReturnId {
 
     public static ProductReturnId fromString(String value) throws DomainException {
         try {
+            if (value == null || value.isEmpty()) {
+                throw new DomainException("El ID de devolución de producto no puede ser nulo o vacío.");
+            }
             UUID.fromString(value);
         } catch (IllegalArgumentException e) {
             throw new DomainException("El ID de devolución de producto no tiene un formato válido: " + value);
