40dcd97cb06587fe1bb86286c2041f1d2af51d90
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-03 19:07:51 -0500

    correcion de saleRepository

--- minerva/src/main/java/com/minerva/domain/repositories/SaleRepository.java
+++ minerva/src/main/java/com/minerva/domain/repositories/SaleRepository.java
@@ -1,17 +1,19 @@
 package com.minerva.domain.repositories;
 
 import com.minerva.domain.entities.customer.CustomerId;
+import com.minerva.domain.entities.sale.PayId;
 import com.minerva.domain.entities.sale.Sale;
+import com.minerva.domain.entities.sale.SaleDetailId;
+import com.minerva.domain.entities.sale.SaleId;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface SaleRepository {
     void save(Sale sale);
-    Optional<Sale> findById(UUID id);
+    Optional<Sale> findById(SaleId id);
     List<Sale> findByCustomerId(CustomerId customerId);
     List<Sale> findAll();
-    List<Sale.SaleDetailDTO> findSaleDetailsById(UUID id);
-    List<Sale.PayDTO> findPaysById(UUID id);
+    List<Sale.SaleDetailDTO> findSaleDetailsById(SaleDetailId id);
+    List<Sale.PayDTO> findPaysById(PayId id);
 }
