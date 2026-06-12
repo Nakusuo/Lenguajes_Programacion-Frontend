8132bfa9b22096403d82d230bd989667a5726458
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-12 15:11:14 -0500

    test restaruado

---
+++ minerva/src/test/java/com/minerva/MinervaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.minerva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinervaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
