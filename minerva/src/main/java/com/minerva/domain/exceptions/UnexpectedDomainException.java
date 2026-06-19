8f4e881d475cb38e008d317a7f4f0e9c240eb86e
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-18 21:59:40 -0500

    se agrego nuevo constructor

--- minerva/src/main/java/com/minerva/domain/exceptions/UnexpectedDomainException.java
+++ minerva/src/main/java/com/minerva/domain/exceptions/UnexpectedDomainException.java
@@ -5,4 +5,8 @@ public class UnexpectedDomainException extends RuntimeException {
     public UnexpectedDomainException(String message, Throwable cause) {
         super(message, cause);
     }
+    public UnexpectedDomainException(String message) {
+        super(message);
+    }
+
 }
