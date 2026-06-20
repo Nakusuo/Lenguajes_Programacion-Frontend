7aa0af1989d8f7d371358e08d6cb059f83dc96f8
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-19 22:52:24 -0500

    nuevo constructor

--- minerva/src/main/java/com/minerva/domain/valueObject/Name.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/Name.java
@@ -4,16 +4,23 @@ import com.minerva.domain.exceptions.DomainException;
 import com.minerva.domain.interfaces.ValueObject;
 
 public class Name extends ValueObject<String> {
-    private static final Integer MIN_LENGTH = 3;
-    private static final Integer MAX_LENGTH = 50;
+    private static final int MIN_LENGTH = 3;
+    private static final int MAX_LENGTH = 50;
 
     public Name(String value) throws DomainException {
         super(value);
-
-        if (value.isBlank()) throw new DomainException("El NOMBRE no puede estar vacío.");
-        if (value.length() < MIN_LENGTH) throw new DomainException("El NOMBRE debe tener al menos " + MIN_LENGTH + " caracteres.");
-        if (value.length() > MAX_LENGTH) throw new DomainException("El NOMBRE no puede exceder los " + MAX_LENGTH + " caracteres.");
-        if (!value.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$")) throw new DomainException("El NOMBRE solo debe contener letras.");
+        validate(value, MIN_LENGTH, MAX_LENGTH);
     }
 
+    public Name(String value, int minLength, int maxLength) throws DomainException {
+        super(value);
+        validate(value, minLength, maxLength);
+    }
+
+    private void validate(String value, int minLength, int maxLength) throws DomainException {
+        if (value.isBlank()) throw new DomainException("El NOMBRE no puede estar vacío.");
+        if (value.length() < minLength) throw new DomainException("El NOMBRE debe tener al menos " + minLength + " caracteres.");
+        if (value.length() > maxLength) throw new DomainException("El NOMBRE no puede exceder los " + maxLength + " caracteres.");
+        if (!value.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$")) throw new DomainException("El NOMBRE solo debe contener letras.");
+    }
 }
