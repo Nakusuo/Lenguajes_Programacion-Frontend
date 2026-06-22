9bdba9e380c5f63ad673bd2a3764c77ae4f9be7a
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-22 15:58:13 -0500

    user

--- minerva/src/main/java/com/minerva/domain/entities/user/User.java
+++ minerva/src/main/java/com/minerva/domain/entities/user/User.java
@@ -3,12 +3,11 @@ package com.minerva.domain.entities.user;
 import java.time.LocalDateTime;
 
 import com.minerva.domain.constants.Role;
-import com.minerva.domain.valueObject.LastName;
-import com.minerva.domain.valueObject.Name;
-import com.minerva.domain.valueObject.Password;
-import com.minerva.domain.valueObject.UserName;
+import com.minerva.domain.interfaces.PasswordHasher;
+import com.minerva.domain.valueObject.*;
 import com.minerva.domain.valueObject.id.DNI;
 import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.exceptions.UnexpectedDomainException;
 import com.minerva.domain.interfaces.Entity;
 
 public class User extends Entity {
@@ -16,25 +15,44 @@ public class User extends Entity {
     private Name names;
     private LastName lastNames;
     private final UserName username;
-    private Password password;
+    private PasswordHash password;
     private Role role;
+    private boolean isActive;
     private final LocalDateTime registrationDate;
 
-    public User(String dni, String names, String lastNames, String username, String password, Role role) throws DomainException {
+    public User( PasswordHasher passwordHasher, String dni, String names, String lastNames, String username, String password, Role role) throws DomainException {
         DNI tempDni = new DNI(dni);
         super(tempDni);
         this.dni = tempDni;
         this.names = new Name(names);
         this.lastNames = new LastName(lastNames);
         this.username = new UserName(username);
-        this.password = new Password(password);
+        this.password = passwordHasher.hash(new Password(password));
         if (role == null) {
             throw new DomainException("El ROL no puede ser nulo.");
         } else {
             this.role = role;
         }
-
+        this.isActive = true;
         this.registrationDate = LocalDateTime.now();
+    }
+
+    public User(String dni, String username, LocalDateTime registrationDate, String names, String lastNames, String password, Role role, boolean isActive) {
+        DNI tempDni;
+        try {
+            tempDni = new DNI(dni);
+            this.dni = tempDni;
+            this.username = new UserName(username);
+            this.names = new Name(names);
+            this.lastNames = new LastName(lastNames);
+            this.password = new PasswordHash(password);
+            this.role = role;
+            this.isActive = isActive;
+            this.registrationDate = registrationDate;
+        } catch (DomainException e) {
+            throw new UnexpectedDomainException("Error al cargar el usuario", e);
+        }
+        super(tempDni);
     }
 
     public DNI getDni() {
@@ -49,20 +67,23 @@ public class User extends Entity {
         return lastNames;
     }
 
-    public UserName getUsername() {
-        return username;
+    public PasswordHash getPassword() {
+        return password;
     }
 
-    public Password getPassword() {
-        return password;
+    public UserName getUsername() {
+        return username;
     }
 
     public Role getRole() {
         return role;
     }
 
+    public boolean isActive() {
+        return isActive;
+    }
+
     public LocalDateTime getRegistrationDate() {
         return registrationDate;
     }
-    
 }
--- minerva/src/main/java/com/minerva/domain/interfaces/PasswordHasher.java
+++ minerva/src/main/java/com/minerva/domain/interfaces/PasswordHasher.java
@@ -1,8 +1,9 @@
 package com.minerva.domain.interfaces;
 
+import com.minerva.domain.valueObject.Password;
 import com.minerva.domain.valueObject.PasswordHash;
 
 public interface PasswordHasher {
-    PasswordHash hash(String rawPassword);
+    PasswordHash hash(Password rawPassword);
     boolean matches(String password);
 }
