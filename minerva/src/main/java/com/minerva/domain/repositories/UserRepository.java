898e3b2a746357127aba266b45a33cbcef3dfe05
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-22 23:45:15 -0500

    userrepo

--- minerva/src/main/java/com/minerva/domain/repositories/UserRepository.java
+++ minerva/src/main/java/com/minerva/domain/repositories/UserRepository.java
@@ -2,12 +2,13 @@ package com.minerva.domain.repositories;
 
 import com.minerva.domain.constants.Role;
 import com.minerva.domain.entities.user.User;
+import com.minerva.domain.entities.user.UserId;
 import com.minerva.domain.valueObject.id.DNI;
 
 public interface UserRepository {
     void save(User user);
 
-    boolean existsById(DNI dni);
-    User findById(DNI dni);
-    Role findRoleByDni(DNI dni);
+    boolean existsById(UserId dni);
+    User findById(UserId dni);
+    Role findRoleByDni(UserId dni);
 }
