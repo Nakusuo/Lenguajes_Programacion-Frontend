db93e7571c5887ad61ebe9c06af437da5fac8110
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-24 12:47:17 -0500

    roles y permisso asosciados

--- minerva/src/main/java/com/minerva/domain/constants/Permission.java
+++ minerva/src/main/java/com/minerva/domain/constants/Permission.java
@@ -1,5 +1,57 @@
 package com.minerva.domain.constants;
 
 public enum Permission {
-    Test
+
+    // Customer - Write
+    CUSTOMER_REGISTER,
+    CUSTOMER_UPDATE_PHONE_NUMBER,
+
+    // Customer - Read
+    CUSTOMER_FIND_BY_ID,
+    CUSTOMER_FIND_BY_PHONE_NUMBER,
+    CUSTOMER_GET_ALL,
+//---------------------------------------------------------
+    // Product - Write
+    PRODUCT_REGISTER,
+    PRODUCT_REGISTER_STOCK_ENTRY,
+    PRODUCT_ASSOCIATE_UNIT_TO_BULK,
+
+    // Product - Read
+    PRODUCT_FIND_BY_ID,
+    PRODUCT_FIND_BY_BAR_CODE,
+    PRODUCT_FIND_ALL,
+
+//---------------------------------------------------------
+
+    // Sale - Write
+    SALE_REGISTER,
+    SALE_ADD_PAYMENT,
+
+    // Sale - Read
+    SALE_FIND_BY_ID,
+    SALE_FIND_BY_CUSTOMER_ID,
+    SALE_FIND_ALL,
+//---------------------------------------------------------
+    // Supplier - Write
+    SUPPLIER_REGISTER,
+    SUPPLIER_UPDATE_PHONE_NUMBER,
+    SUPPLIER_UPDATE_RUC,
+
+    // Supplier - Read
+    SUPPLIER_FIND_ALL,
+    SUPPLIER_FIND_BY_ID,
+    SUPPLIER_FIND_BY_RUC,
+    SUPPLIER_FIND_BY_PHONE_NUMBER,
+//---------------------------------------------------------
+
+    // User - Write
+    USER_REGISTER,
+
+    // User - Auth
+    USER_AUTHENTICATE,
+
+    // User - Read
+    USER_FIND_BY_USERNAME,
+    USER_FIND_BY_ID,
+    USER_FIND_ALL
 }
--- minerva/src/main/java/com/minerva/domain/constants/Role.java
+++ minerva/src/main/java/com/minerva/domain/constants/Role.java
@@ -5,13 +5,37 @@ import java.util.Set;
 public enum Role {
 
     ADMIN(Set.of(
-            Permission.Test
+            Permission.values()
     )),
     VENDEDOR(Set.of(
-            Permission.Test
+            Permission.CUSTOMER_REGISTER,
+            Permission.CUSTOMER_UPDATE_PHONE_NUMBER,
+            Permission.CUSTOMER_FIND_BY_ID,
+            Permission.CUSTOMER_FIND_BY_PHONE_NUMBER,
+            Permission.CUSTOMER_GET_ALL,
+            Permission.PRODUCT_FIND_BY_ID,
+            Permission.PRODUCT_FIND_BY_BAR_CODE,
+            Permission.PRODUCT_FIND_ALL,
+            Permission.SALE_REGISTER,
+            Permission.SALE_ADD_PAYMENT,
+            Permission.SALE_FIND_BY_ID,
+            Permission.SALE_FIND_BY_CUSTOMER_ID,
+            Permission.SALE_FIND_ALL
     )),
     ALMACENISTA(Set.of(
-            Permission.Test
+            Permission.PRODUCT_REGISTER,
+            Permission.PRODUCT_REGISTER_STOCK_ENTRY,
+            Permission.PRODUCT_ASSOCIATE_UNIT_TO_BULK,
+            Permission.PRODUCT_FIND_BY_ID,
+            Permission.PRODUCT_FIND_BY_BAR_CODE,
+            Permission.PRODUCT_FIND_ALL,
+            Permission.SUPPLIER_REGISTER,
+            Permission.SUPPLIER_UPDATE_PHONE_NUMBER,
+            Permission.SUPPLIER_UPDATE_RUC,
+            Permission.SUPPLIER_FIND_ALL,
+            Permission.SUPPLIER_FIND_BY_ID,
+            Permission.SUPPLIER_FIND_BY_RUC,
+            Permission.SUPPLIER_FIND_BY_PHONE_NUMBER
     ));
 
     private final Set<Permission> permissions;
