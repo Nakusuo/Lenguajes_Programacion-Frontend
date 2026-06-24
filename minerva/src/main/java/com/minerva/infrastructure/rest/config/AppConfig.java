0d809751154b54fb13edd452e44e232674b49e54
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-23 22:13:47 -0500

    beans

--- minerva/src/main/java/com/minerva/infrastructure/rest/config/AppConfig.java
+++ minerva/src/main/java/com/minerva/infrastructure/rest/config/AppConfig.java
@@ -1,28 +1,11 @@
 package com.minerva.infrastructure.rest.config;
 
+import com.minerva.application.service.*;
+import com.minerva.domain.interfaces.PasswordHasher;
+import com.minerva.domain.repositories.*;
+import com.minerva.infrastructure.adapter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.minerva.application.service.CustomerService;
-import com.minerva.application.service.ProductService;
-import com.minerva.application.service.SaleService;
-import com.minerva.application.service.SupplierService;
-import com.minerva.domain.repositories.CustomerRepository;
-import com.minerva.domain.repositories.ProductRepository;
-import com.minerva.domain.repositories.SaleRepository;
-import com.minerva.domain.repositories.SupplierRepository;
-import com.minerva.infrastructure.adapter.CustomerRepositoryAdapter;
-import com.minerva.infrastructure.adapter.ProductRepositoryAdapter;
-import com.minerva.infrastructure.adapter.SaleRepositoryAdapter;
-import com.minerva.infrastructure.adapter.SupplierRepositoryAdapter;
-import com.minerva.infrastructure.persistence.repository.JpaCustomerRepository;
-import com.minerva.infrastructure.persistence.repository.JpaPayRepository;
-import com.minerva.infrastructure.persistence.repository.JpaProductRepository;
-import com.minerva.infrastructure.persistence.repository.JpaSaleDetailRepository;
-import com.minerva.infrastructure.persistence.repository.JpaSaleRepository;
-import com.minerva.infrastructure.persistence.repository.JpaStockEntryRepository;
-import com.minerva.infrastructure.persistence.repository.JpaSupplierRepository;
-import com.minerva.infrastructure.persistence.repository.JpaUnitToBulkRepository;
 
 @Configuration
 public class AppConfig {
@@ -32,24 +15,8 @@ public class AppConfig {
     }   
 
     @Bean
-    public CustomerRepository customerRepository(JpaCustomerRepository customerRepository) {
-        return new CustomerRepositoryAdapter(customerRepository);
-    }
-
-    @Bean
     public ProductService productService(ProductRepository productRepository, SupplierRepository supplierRepository) {
         return new ProductService(productRepository, supplierRepository);
-    }
-
-    @Bean
-    public ProductRepository productRepository(JpaProductRepository jpaProductRepository, JpaStockEntryRepository jpaStockEntryRepository, JpaUnitToBulkRepository jpaUnitToBulkRepository) {
-        return new ProductRepositoryAdapter(jpaProductRepository, jpaStockEntryRepository, jpaUnitToBulkRepository);
-    }
-
-
-    @Bean
-    public SupplierRepository supplierRepository(JpaSupplierRepository jpaSupplierRepository) {
-        return new SupplierRepositoryAdapter(jpaSupplierRepository);
     }
 
     @Bean
@@ -58,14 +25,17 @@ public class AppConfig {
     }
 
     @Bean
-    public SaleRepository saleRepository(JpaSaleRepository jpaSaleRepository, JpaPayRepository jpaPayRepository, JpaSaleDetailRepository jpaSaleDetailRepository) {
-        return new SaleRepositoryAdapter(jpaSaleRepository, jpaPayRepository, jpaSaleDetailRepository);
-    }
-
-    @Bean
     public SupplierService supplierService(SupplierRepository supplierRepository) {
         return new SupplierService(supplierRepository);
     }
+
+    @Bean
+    public UserService userService(UserRepository userRepository, PasswordHasher passwordHasher) {
+        return new UserService(userRepository, passwordHasher);
+    }
+
+    @Bean
+    public PasswordHasher passwordHasher(){
+        return new PasswordHasherAdapter();
+    }
 }
-// Consider defining a bean of type 'com.minerva.application.service.CustomerService' 
-// in your configuration.
