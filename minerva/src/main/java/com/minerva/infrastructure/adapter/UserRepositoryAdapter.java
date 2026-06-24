796f7565e028f92fb448b3794ddc3a238fbbd879
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-24 17:01:29 -0500

    userrepositor

--- minerva/src/main/java/com/minerva/infrastructure/adapter/UserRepositoryAdapter.java
+++ minerva/src/main/java/com/minerva/infrastructure/adapter/UserRepositoryAdapter.java
@@ -2,20 +2,30 @@ package com.minerva.infrastructure.adapter;
 
 import com.minerva.domain.entities.user.User;
 import com.minerva.domain.entities.user.UserId;
+import com.minerva.domain.entities.userAction.UserAction;
 import com.minerva.domain.repositories.UserRepository;
-import com.minerva.domain.valueObject.UserName;
+import com.minerva.domain.valueObject.DNI;
+import com.minerva.infrastructure.persistence.entity.UserActionEntity;
 import com.minerva.infrastructure.persistence.entity.UserEntity;
+import com.minerva.infrastructure.persistence.repository.JpaUserActionRepository;
 import com.minerva.infrastructure.persistence.repository.JpaUserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public class UserRepositoryAdapter implements UserRepository {
-    private final JpaUserRepository jpaUserRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    public UserRepositoryAdapter(JpaUserRepository jpaUserRepository) {
+    private final JpaUserRepository jpaUserRepository;
+    private final JpaUserActionRepository jpaUserActionRepository;
+
+    public UserRepositoryAdapter(JpaUserRepository jpaUserRepository, JpaUserActionRepository jpaUserActionRepository) {
         this.jpaUserRepository = jpaUserRepository;
+        this.jpaUserActionRepository = jpaUserActionRepository;
     }
 
     @Override
@@ -24,23 +34,23 @@ public class UserRepositoryAdapter implements UserRepository {
     }
 
     @Override
+    public void save(UserAction userAction) {
+        jpaUserActionRepository.save(toEntity(userAction));
+    }
+
+    @Override
     public boolean existsById(UserId dni) {
         return jpaUserRepository.existsById(dni.value());
     }
 
     @Override
-    public boolean existsByUsername(UserName username) {
-        return jpaUserRepository.existsByUsername(username.value);
+    public boolean existsByDNI(DNI dni) {
+        return jpaUserRepository.existsByDNI(dni.value);
     }
 
     @Override
     public Optional<User> findById(UserId dni) {
         return jpaUserRepository.findById(dni.value()).map(this::toDomain);
-    }
-
-    @Override
-    public Optional<User> findByUsername(UserName username) {
-        return jpaUserRepository.findByUsername(username.value).map(this::toDomain);
     }
 
     private User toDomain(UserEntity userEntity) {
@@ -51,21 +61,34 @@ public class UserRepositoryAdapter implements UserRepository {
                 userEntity.getUsername(),
                 userEntity.getPassword(),
                 userEntity.getRole(),
-                userEntity.getIsActive(),
+                userEntity.isActive(),
                 userEntity.getRegistrationDate()
         );
     }
 
     private UserEntity toEntity(User user) {
         return new UserEntity(
+                user.getUsername().value,
                 user.getDni().value,
                 user.getNames().value,
                 user.getLastNames().value,
-                user.getUsername().value,
                 user.getPasswordHash().value,
                 user.getRole(),
                 user.isActive(),
                 user.getRegistrationDate()
+        );
+    }
+
+    private UserActionEntity toEntity(UserAction userAction) {
+        UserEntity userEntity = entityManager.getReference(UserEntity.class, userAction.getUserName());
+
+
+        return new UserActionEntity(
+                userAction.getId().asString(),
+                userEntity,
+                userAction.getPermission(),
+                userAction.getEntityId().asString(),
+                userAction.getRegistrationDate()
         );
     }
 }
