e19446edda0f2f62a783c96cfb0d3e120035806e
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-24 17:01:04 -0500

    jpa

--- minerva/src/main/java/com/minerva/infrastructure/persistence/repository/JpaUserRepository.java
+++ minerva/src/main/java/com/minerva/infrastructure/persistence/repository/JpaUserRepository.java
@@ -2,11 +2,8 @@ package com.minerva.infrastructure.persistence.repository;
 
 import com.minerva.infrastructure.persistence.entity.UserEntity;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, String> {
-    boolean existsByUsername(String username);
-    Optional<UserEntity> findByUsername(String username);
+    boolean existsByDNI(String dni);
 }
