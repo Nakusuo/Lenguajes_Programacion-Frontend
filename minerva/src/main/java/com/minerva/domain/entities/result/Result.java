4f08b70a65ed2d04058c924630d484f8fe17458a
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-07-08 01:02:32 -0500

    domainError

--- minerva/src/main/java/com/minerva/domain/entities/result/Result.java
+++ minerva/src/main/java/com/minerva/domain/entities/result/Result.java
@@ -2,49 +2,42 @@ package com.minerva.domain.entities.result;
 
 import java.util.Optional;
 
-import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.constants.DomainError;
 
 // Nota: chat gpt recomienda usar la palbra fuilure en vez de fail, porque dice que failure es sutatntivo
 public class Result<D> {
     private final boolean success;
     private final String message;
     private final D data;
-    private final DomainException exception;
+    private final DomainError domainError;
 
-    private Result(boolean success, String message, D data) {
+    private Result(boolean success, String message, D data, DomainError domainError) {
         this.success = success;
         this.message = message;
         this.data = data;
-        this.exception = null;
-    }
-
-    private Result(DomainException exception) {
-        this.success = false;
-        this.message = exception.getMessage();
-        this.data = null;
-        this.exception = exception;
+        this.domainError = domainError;
     }
 
     public static <D> Result<D> success(D data) {
-        return new Result<>(true, "", data);
+        return new Result<>(true, "", data, null);
     }
 
     public static <D> Result<D> success(D data, String message) {
-        return new Result<>(true, message, data);
+        return new Result<>(true, message, data, null);
     }
 
     public static <D> Result<D> fail(String message) {
-        return new Result<>(false, message, null);
+        return new Result<>(false, message, null, null);
     }
 
-    public static <D, E extends DomainException> Result<D> fail(E exception) {
-        return new Result<>(exception);
+    public static <D> Result<D> fail(DomainError domainError) {
+        return new Result<>(false, domainError.name(), null, domainError);
     }
 
     public boolean isSuccess() { return success; }
     public boolean isFail() {return !success;}
     public String getMessage() { return message; }
     public D getData() { return data; }
-    public Optional<DomainException> getException() { return Optional.ofNullable(exception); }
+    public Optional<DomainError> getDomainError() { return Optional.ofNullable(domainError); }
 
 }
--- minerva/src/main/java/com/minerva/domain/exceptions/UnauthorizedActionException.java
+++
@@ -1,8 +0,0 @@
-package com.minerva.domain.exceptions;
-
-public class UnauthorizedActionException extends DomainException {
-
-    public UnauthorizedActionException(String message) {
-        super(message);
-    }
-}
