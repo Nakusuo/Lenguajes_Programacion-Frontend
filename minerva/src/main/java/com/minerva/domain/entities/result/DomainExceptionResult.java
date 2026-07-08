4608ae628de626baa01428af49110b8a8c8d77ae
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-07-08 00:08:41 -0500

    mayor control

--- minerva/src/main/java/com/minerva/domain/entities/result/DomainExceptionResult.java
+++ minerva/src/main/java/com/minerva/domain/entities/result/DomainExceptionResult.java
@@ -2,23 +2,23 @@ package com.minerva.domain.entities.result;
 
 import com.minerva.domain.exceptions.DomainException;
 
-public class DomainExceptionResult<D> extends Result<D> {
-    private final DomainException domainException;
+public class DomainExceptionResult<D, E extends DomainException> extends Result<D> {
+    private final E domainException;
 
-    private DomainExceptionResult(boolean success, DomainException exception, D data) {
+    private DomainExceptionResult(boolean success, E exception, D data) {
         super(success, exception.getMessage(), data);
         this.domainException = exception;
     }
 
-    public static <D> Result<D> success(D data, DomainException exception) {
+    public static <D, E extends DomainException> Result<D> success(D data, E exception) {
         return new DomainExceptionResult<>(true, exception, data);
     }
 
-    public static <D> Result<D> fail(DomainException exception) {
+    public static <D, E extends DomainException> Result<D> fail(E exception) {
         return new DomainExceptionResult<>(false, exception, null);
     }
 
-    public DomainException getDomainException() {
+    public E getDomainException() {
         return domainException;
     }
 }
