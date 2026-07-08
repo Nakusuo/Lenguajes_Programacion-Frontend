f7c816719cab28429ea881a8308536a2fe8cbba6
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-07-08 01:13:45 -0500

    e

---
+++ minerva/src/main/java/com/minerva/domain/exceptions/UnauthorizedActionException.java
@@ -0,0 +1,7 @@
+package com.minerva.domain.exceptions;
+
+public class UnauthorizedActionException extends DomainException {
+    public UnauthorizedActionException(String message) {
+        super(message);
+    }
+}
