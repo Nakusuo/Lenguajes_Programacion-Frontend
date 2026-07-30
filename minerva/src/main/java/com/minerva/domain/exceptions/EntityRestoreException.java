cb5cbefd092934e5df1de1b0c1451eea216e43fe
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-07-29 21:06:26 -0500

    xe

---
+++ minerva/src/main/java/com/minerva/domain/exceptions/EntityRestoreException.java
@@ -0,0 +1,12 @@
+package com.minerva.domain.exceptions;
+
+public class EntityRestoreException extends UnexpectedDomainException {
+
+    public EntityRestoreException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityRestoreException(String message) {
+        super(message);
+    }
+}
