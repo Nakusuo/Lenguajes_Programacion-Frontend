f3204cf1beef9ad108e6547093d9f110c8fe1e11
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-07-30 21:34:49 -0500

    idproduct

--- minerva/src/main/java/com/minerva/domain/entities/product/ProductId.java
+++ minerva/src/main/java/com/minerva/domain/entities/product/ProductId.java
@@ -2,5 +2,7 @@ package com.minerva.domain.entities.product;
 
 import com.minerva.domain.valueObject.id.Id;
 
-public interface ProductId extends Id<String> {
+import java.util.UUID;
+
+public interface ProductId extends Id<UUID> {
 }
