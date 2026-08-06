a303d5178f69ae3687bfac9f67b9b9205594a572
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-06 01:32:48 -0500

    ruc

--- minerva/src/main/java/com/minerva/domain/valueObject/RUC.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/RUC.java
@@ -1,15 +1,15 @@
 package com.minerva.domain.valueObject;
 
-import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.exceptions.InvalidDomainArgumentException;
 
 public class RUC extends ValueObject<String> {
     private static final int LENGTH = 11;
 
-    public RUC(String value) throws DomainException {
+    public RUC(String value) throws InvalidDomainArgumentException {
         super(value);
 
-        if (value.length() != LENGTH) throw new DomainException("El RUC debe tener exactamente " + LENGTH + " caracteres.");
-        if (!value.matches("^\\d+$")) throw new DomainException("El RUC debe contener solo números.");
+        if (value.length() != LENGTH) throw new InvalidDomainArgumentException("El RUC debe tener exactamente " + LENGTH + " caracteres.");
+        if (!value.matches("^\\d+$")) throw new InvalidDomainArgumentException("El RUC debe contener solo números.");
     }
 
 }
