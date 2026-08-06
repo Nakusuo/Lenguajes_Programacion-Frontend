b7ce367186f6a9a11f5c31f500e063f08448941b
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-06 01:26:54 -0500

    dni

--- minerva/src/main/java/com/minerva/domain/valueObject/DNI.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/DNI.java
@@ -1,16 +1,17 @@
 package com.minerva.domain.valueObject;
 
-import com.minerva.domain.exceptions.DomainException;
+
+import com.minerva.domain.exceptions.InvalidDomainArgumentException;
 
 public class DNI extends ValueObject<String> {
 
     private static final int LENGTH = 8;
 
-    public DNI(String value) throws DomainException {
+    public DNI(String value) throws InvalidDomainArgumentException {
         super(value);
 
-        if (value.isBlank()) throw new DomainException("El DNI no puede estar vacío.");
-        if (!value.matches("^\\d+$")) throw new DomainException("El DNI solo puede contener números.");
-        if (value.length() != LENGTH) throw new DomainException("El DNI debe tener exactamente " + LENGTH + " dígitos.");
+        if (value.isBlank()) throw new InvalidDomainArgumentException("El DNI no puede estar vacío.");
+        if (!value.matches("^\\d+$")) throw new InvalidDomainArgumentException("El DNI solo puede contener números.");
+        if (value.length() != LENGTH) throw new InvalidDomainArgumentException("El DNI debe tener exactamente " + LENGTH + " dígitos.");
     }
 }
