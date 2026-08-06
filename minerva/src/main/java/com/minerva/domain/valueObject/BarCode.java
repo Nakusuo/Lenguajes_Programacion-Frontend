ef9b5b2e1cc284bbf3a88f4065c26070c21448f1
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-06 01:26:16 -0500

    a

--- minerva/src/main/java/com/minerva/domain/valueObject/BarCode.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/BarCode.java
@@ -1,14 +1,14 @@
 package com.minerva.domain.valueObject;
 
-import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.exceptions.InvalidDomainArgumentException;
 
 public class BarCode extends ValueObject<String> {
 
-    public BarCode(String value) throws DomainException {
+    public BarCode(String value) throws InvalidDomainArgumentException {
         super(value);
 
-        if (value.isBlank()) throw new DomainException("El código de barras no puede estar vacío.");
-        if (value.length() != 13) throw new DomainException("El código de barras debe tener 13 dígitos.");
-        if (!value.matches("^\\d+$")) throw new DomainException("El código de barras solo puede contener números.");
+        if (value.isBlank()) throw new InvalidDomainArgumentException("El código de barras no puede estar vacío.");
+        if (value.length() != 13) throw new InvalidDomainArgumentException("El código de barras debe tener 13 dígitos.");
+        if (!value.matches("^\\d+$")) throw new InvalidDomainArgumentException("El código de barras solo puede contener números.");
     }
 }
