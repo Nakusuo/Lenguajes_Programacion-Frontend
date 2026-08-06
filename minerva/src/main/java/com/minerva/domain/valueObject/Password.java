273a13b46867d80aadb69ed4cf2d57bf7b5ed24d
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-06 01:29:55 -0500

    a

--- minerva/src/main/java/com/minerva/domain/valueObject/Password.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/Password.java
@@ -1,18 +1,18 @@
 package com.minerva.domain.valueObject;
 
-import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.exceptions.InvalidDomainArgumentException;
 
 public final class Password extends ValueObject<String> {
 
     private static final int MIN_LENGTH = 8;
     private static final int MAX_LENGTH = 100;
 
-    public Password(String value) throws DomainException {
+    public Password(String value) throws InvalidDomainArgumentException {
         super(value);
         
-        if (value.isBlank()) throw new DomainException("El PASSWORD no puede estar vacío.");
-        if (value.length() < MIN_LENGTH) throw new DomainException("El PASSWORD debe tener al menos " + MIN_LENGTH + " caracteres.");
-        if (value.length() > MAX_LENGTH)throw new DomainException("El PASSWORD no puede exceder los " + MAX_LENGTH + " caracteres.");
+        if (value.isBlank()) throw new InvalidDomainArgumentException("El PASSWORD no puede estar vacío.");
+        if (value.length() < MIN_LENGTH) throw new InvalidDomainArgumentException("El PASSWORD debe tener al menos " + MIN_LENGTH + " caracteres.");
+        if (value.length() > MAX_LENGTH)throw new InvalidDomainArgumentException("El PASSWORD no puede exceder los " + MAX_LENGTH + " caracteres.");
     }
 
 }
