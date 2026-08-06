f83aa7aad65316dd48790d2fdfcf961b2e12a0af
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-06 01:31:11 -0500

    product name

--- minerva/src/main/java/com/minerva/domain/valueObject/ProductName.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/ProductName.java
@@ -1,18 +1,18 @@
 package com.minerva.domain.valueObject;
 
-import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.exceptions.InvalidDomainArgumentException;
 
 public class ProductName extends ValueObject<String> {
     private static final int MIN_LENGTH = 3;
     private static final int MAX_LENGTH = 100;
 
-    public ProductName(String value) throws DomainException {
+    public ProductName(String value) throws InvalidDomainArgumentException {
         super(value);
 
-        if (value.isBlank()) throw new DomainException("El nombre del producto no puede estar vacío.");
-        if (value.length() < MIN_LENGTH) throw new DomainException("El nombre del producto debe tener al menos " + MIN_LENGTH + " caracteres.");
-        if (value.length() > MAX_LENGTH) throw new DomainException("El nombre del producto no puede tener más de " + MAX_LENGTH + " caracteres.");
-        if (!value.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 ]+$")) throw new DomainException("El nombre del producto solo puede contener letras y números.");
+        if (value.isBlank()) throw new InvalidDomainArgumentException("El nombre del producto no puede estar vacío.");
+        if (value.length() < MIN_LENGTH) throw new InvalidDomainArgumentException("El nombre del producto debe tener al menos " + MIN_LENGTH + " caracteres.");
+        if (value.length() > MAX_LENGTH) throw new InvalidDomainArgumentException("El nombre del producto no puede tener más de " + MAX_LENGTH + " caracteres.");
+        if (!value.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 ]+$")) throw new InvalidDomainArgumentException("El nombre del producto solo puede contener letras y números.");
     }
 
 }
