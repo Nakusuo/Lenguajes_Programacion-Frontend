176f91680572fabf093bdb51a329c770d64fe822
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-06 01:27:14 -0500

    las

--- minerva/src/main/java/com/minerva/domain/valueObject/LastName.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/LastName.java
@@ -1,18 +1,18 @@
 package com.minerva.domain.valueObject;
 
-import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.exceptions.InvalidDomainArgumentException;
 
 public class LastName extends ValueObject<String> {
     private static final int MIN_LENGTH = 3;
     private static final int MAX_LENGTH = 50;
 
-    public LastName(String value) throws DomainException {
+    public LastName(String value) throws InvalidDomainArgumentException {
         super(value);
 
-        if (value.isBlank()) throw new DomainException("El APELLIDO no puede estar vacío.");
-        if (value.length() < MIN_LENGTH) throw new DomainException("El APELLIDO debe tener al menos " + MIN_LENGTH + " caracteres.");
-        if (value.length() > MAX_LENGTH) throw new DomainException("El APELLIDO no puede exceder los " + MAX_LENGTH + " caracteres.");
-        if (!value.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$")) throw new DomainException("El APELLIDO solo debe contener letras.");
+        if (value.isBlank()) throw new InvalidDomainArgumentException("El APELLIDO no puede estar vacío.");
+        if (value.length() < MIN_LENGTH) throw new InvalidDomainArgumentException("El APELLIDO debe tener al menos " + MIN_LENGTH + " caracteres.");
+        if (value.length() > MAX_LENGTH) throw new InvalidDomainArgumentException("El APELLIDO no puede exceder los " + MAX_LENGTH + " caracteres.");
+        if (!value.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$")) throw new InvalidDomainArgumentException("El APELLIDO solo debe contener letras.");
     }
     
 }
