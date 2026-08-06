137b3cb0d2053be6de89c68296f40c2918cf05c6
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-06 01:30:50 -0500

    a

--- minerva/src/main/java/com/minerva/domain/valueObject/PhoneNumber.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/PhoneNumber.java
@@ -1,16 +1,16 @@
 package com.minerva.domain.valueObject;
 
-import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.exceptions.InvalidDomainArgumentException;
 
 // MEJORAS: Los regex
 public class PhoneNumber extends ValueObject<String> {
     private static final int LENGTH = 9;
 
-    public PhoneNumber(String value) throws DomainException {
+    public PhoneNumber(String value) throws InvalidDomainArgumentException {
         super(value);
 
-        if (value.length() != LENGTH) throw new DomainException("El número de teléfono debe tener " + LENGTH + " dígitos.");
-        if (!value.matches("^\\d+$")) throw new DomainException("El número de teléfono solo puede contener números.");
+        if (value.length() != LENGTH) throw new InvalidDomainArgumentException("El número de teléfono debe tener " + LENGTH + " dígitos.");
+        if (!value.matches("^\\d+$")) throw new InvalidDomainArgumentException("El número de teléfono solo puede contener números.");
     }
 
 }
