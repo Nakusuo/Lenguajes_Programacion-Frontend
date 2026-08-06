12842bd1febecbb4233452f7c5dbe7ca689c3fac
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-06 01:29:28 -0500

    name

--- minerva/src/main/java/com/minerva/domain/valueObject/Name.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/Name.java
@@ -1,25 +1,25 @@
 package com.minerva.domain.valueObject;
 
-import com.minerva.domain.exceptions.DomainException;
+import com.minerva.domain.exceptions.InvalidDomainArgumentException;
 
 public class Name extends ValueObject<String> {
     private static final int MIN_LENGTH = 3;
     private static final int MAX_LENGTH = 50;
 
-    public Name(String value) throws DomainException {
+    public Name(String value) throws InvalidDomainArgumentException {
         super(value);
         validate(value, MIN_LENGTH, MAX_LENGTH);
     }
 
-    public Name(String value, int minLength, int maxLength) throws DomainException {
+    public Name(String value, int minLength, int maxLength) throws InvalidDomainArgumentException {
         super(value);
         validate(value, minLength, maxLength);
     }
 
-    private void validate(String value, int minLength, int maxLength) throws DomainException {
-        if (value.isBlank()) throw new DomainException("El NOMBRE no puede estar vacío.");
-        if (value.length() < minLength) throw new DomainException("El NOMBRE debe tener al menos " + minLength + " caracteres.");
-        if (value.length() > maxLength) throw new DomainException("El NOMBRE no puede exceder los " + maxLength + " caracteres.");
-        if (!value.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$")) throw new DomainException("El NOMBRE solo debe contener letras.");
+    private void validate(String value, int minLength, int maxLength) throws InvalidDomainArgumentException {
+        if (value.isBlank()) throw new InvalidDomainArgumentException("El NOMBRE no puede estar vacío.");
+        if (value.length() < minLength) throw new InvalidDomainArgumentException("El NOMBRE debe tener al menos " + minLength + " caracteres.");
+        if (value.length() > maxLength) throw new InvalidDomainArgumentException("El NOMBRE no puede exceder los " + maxLength + " caracteres.");
+        if (!value.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$")) throw new InvalidDomainArgumentException("El NOMBRE solo debe contener letras.");
     }
 }
